993f05fb7237652a0b29cbb2741bedb0ff3b3018
tejasghlade <tejasghlade@gmail.com>
2025-02-19 16:41:00 +0530

    Add static file serving from the public directory

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`))
+
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👍');
     next();
