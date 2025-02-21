4eae8f9cdd050e97e6f5cda4ce743ecfeff0812d
tejasghlade <tejasghlade@gmail.com>
2025-02-21 17:29:45 +0530

    add alias for top 5 cheap tours with sorting and field selection

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,11 +5,15 @@ const {
   updateTour,
   deleteTour,
   getTour,
+  aliasTopTours,
 } = require('../controller/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkId); // middlewares
+
+// aliasing route
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 // Tour routes
 router.route('/').get(getAllTours).post(createTour);
