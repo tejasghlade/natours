46d8e940434986f16279413c040a8c1723494eda
tejasghlade <tejasghlade@gmail.com>
2025-02-22 00:05:53 +0530

    add getTourStats method to aggregate tour statistics and update routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,7 @@ const {
   deleteTour,
   getTour,
   aliasTopTours,
+  getTourStats,
 } = require('../controller/tourController');
 
 const router = express.Router();
@@ -14,6 +15,8 @@ const router = express.Router();
 
 // aliasing route
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 // Tour routes
 router.route('/').get(getAllTours).post(createTour);
