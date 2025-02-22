fc6dcd1205e1633ca0221d7d044583e5edf33d11
tejasghlade <tejasghlade@gmail.com>
2025-02-22 16:48:33 +0530

    add getMonthlyPlan method to retrieve monthly tour statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
   getTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controller/tourController');
 
 const router = express.Router();
@@ -17,6 +18,7 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 // Tour routes
 router.route('/').get(getAllTours).post(createTour);
