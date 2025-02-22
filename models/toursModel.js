b8462d3ad622f07f2b7da9d6e75c9cadf3e69963
tejasghlade <tejasghlade@gmail.com>
2025-02-22 18:09:54 +0530

    query middleware

--- models/toursModel.js
+++ models/toursModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -53,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       // select: false, If we want to hide the fields when we get the response
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: {
@@ -64,9 +70,40 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
-// virtual properties like eg: when we have date of birth then we can calculate his age.
+// todo ::virtual properties like eg: when we have date of birth then we can calculate his age.
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// todo :: Document middleware : runs before  .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+// todo :: Query Middleware
+// tourSchema.pre('find', function (next) {
+tourSchema.pre('/^find/', function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
